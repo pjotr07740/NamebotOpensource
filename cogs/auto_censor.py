fea6ff4e39dfe795b5d31d66c70618f2efbfe9d2
pjotr07740 <pjotrvanveen06@gmail.com>
2020-08-21 15:46:58 +0200

    Fix for on_member_update

--- cogs/auto_censor.py
+++ cogs/auto_censor.py
@@ -41,7 +41,10 @@ class AutoCensor(commands.Cog):
             for bad_word in bad_words:
                 if nick.count(bad_word) > 0:
                     username_change_bot.add_field(name="User", value=f"```{after}```")
-                    await after.edit(nick="[NAME REDACTED]", reason="Auto change")
+                    try:
+                        await after.edit(nick="[NAME REDACTED]", reason="Auto change")
+                    except:
+                        pass
                     await after.send("You're contains profanity if you think this is an error dm `Pjotr#1418`!")
                     await channel.send(content=None, embed=username_change_bot)
                     break
